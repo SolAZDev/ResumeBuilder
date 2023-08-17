ed05a6faec1b294e6978f42af3b2f817699e1a9a
SolAZDev <SolAZDev@gmail.com>
2023-08-17 07:54:04 -0400

    changed recomended extensions

---
+++ src/pages/ProfileEditor.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+q-page
+  q-stepper(v-model="step")
+  q-stepper-
+</template>
+<script setup lang="ts">
+import { ref } from 'vue';
+import type {
+  ResumeProfile,
+  Award,
+  Education,
+  Tooling,
+  Work,
+} from '../data/resume.model';
+
+const step = ref(0);
+const resume = ref({
+  icon: '',
+  name: '',
+  subtitle: '',
+  location: '',
+  email: '',
+  number: '',
+  objective: '',
+  skills: [],
+  frameworks: [] as Tooling[],
+  software: [] as Tooling[],
+  education: [] as Education[],
+  awards: [] as Award[],
+  work: [] as Work[],
+} as ResumeProfile);
+
+function importProfile() {
+  return;
+}
+</script>
